d9ecc8c7ba69b72adcd475c7af4bccb37c22e0c0
rishanhem <128331434+rishanhem@users.noreply.github.com>
2024-09-27 07:23:47 -0500

    Create va.py

---
+++ va.py
@@ -0,0 +1,71 @@
+
+import pyttsx3
+import webbrowser
+import smtplib
+import random
+import speech_recognition as sr
+import wikipedia
+import datetime
+import wolframalpha
+import os
+import sys
+
+engine = pyttsx3.init('sapi5')
+
+client = wolframalpha.Client('L8YW7X-XTWE42A43L')
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[len(voices)-2].id)
+
+def speak(audio):
+    print('Computer: ' + audio)
+    engine.say(audio)
+    engine.runAndWait()
+
+def greetMe():
+    currentH = int(datetime.datetime.now().hour)
+    if currentH >= 0 and currentH < 12:
+        speak('Good Morning!')
+
+    if currentH >= 12 and currentH < 18:
+        speak('Good Afternoon!')
+
+    if currentH >= 18 and currentH !=0:
+        speak('Good Evening!')
+
+greetMe()
+
+speak('Hello, How would you like to move the robot arm!')
+
+
+
+def myCommand():
+   
+    r = sr.Recognizer()                                                                                   
+    with sr.Microphone() as source:                                                                       
+        print("Listening...")
+        r.pause_threshold =  .5
+        audio = r.listen(source)
+    try:
+        query = r.recognize_google(audio, language='en-in')
+        print('User: ' + query + '\n')
+        
+    except sr.UnknownValueError:
+        speak('Sorry ! I didn\'t get that! Try typing the command!')
+        query = str(input('Command: '))
+
+    return query
+        
+
+if __name__ == '__main__':
+
+    while True:
+    
+        query = myCommand();
+        query = query.lower()
+        
+        if 'move' in query:
+            speak("How much would you like it to move")
+        
+        speak('Next Command! ')
+        
